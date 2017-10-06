89ac0a1ca59d5fc57dfbe705df0bf22fdad7815a
Jose DAW <Jose DAW@DESKTOP-KR17KU9>
2017-10-06 18:05:36 +0200

    Commit Inicial

---
+++ src/persona/Persona.java
@@ -0,0 +1,17 @@
+
+package persona;
+
+/**
+ *
+ * @author Jose DAW
+ */
+public class Persona {
+private String nombre;
+private String apellido;   
+
+    public Persona (Persona persona){
+        this.nombre = persona.nombre;
+         this.nombre = persona.apellido;
+    }
+    
+}
